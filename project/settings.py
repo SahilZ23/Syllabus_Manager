e4b3e6afac26be2088d2d540e6fa5906edfdbc1d
Sahil Zaveri <99435767+SahilZ23@users.noreply.github.com>
2023-12-25 23:45:36 -0600

    Update settings.py
    Removed Key references

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'etngh7%62q2i0r9lsoyn9x1z@&ghjao+gfodtse9_vu9mbmms('
+SECRET_KEY = 'DJANGO APP Secret Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
